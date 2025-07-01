8c17c9ffe3b564ef83b6a046967551212fa7925e
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2025-07-01 19:39:36 +0530

    update

--- 1-Operators/amazon-cart-total.js
+++ 1-Operators/amazon-cart-total.js
@@ -9,5 +9,5 @@ product_total = basketball + tShirt *2;
 tax_total = product_total * (tax_percentage / 100);
 final_total = product_total + tax_total;
 console.log("Product Total: " + product_total + "$");
-console.log("Tax Total: " + tax_total + "$");
+console.log("Tax Total(10%): " + tax_total + "$");
 console.log("Order Total: " + final_total+"$");
