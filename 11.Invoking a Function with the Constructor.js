83f322d7ef50d9d9a1c0939189b29940a5d4aea7
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2025-07-10 09:33:34 +0530

    update

---
+++ 11.Invoking a Function with the Constructor.js
@@ -0,0 +1,11 @@
+// This is a function constructor:
+function myFunction(arg1, arg2) {
+  this.firstName = arg1;
+  this.lastName  = arg2;
+}
+
+// This creates a new object
+const myObj = new myFunction("John", "Doe");
+
+// This will return "John"
+console.log(myObj.firstName);
---
+++ 9.find max and sum using function.js
@@ -0,0 +1,25 @@
+x = findMax(1, 123, 500, 115, 44, 88);
+
+function findMax() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
+}
+
+console.log("Max Number is : " + x);
+
+
+x = sumAll(100, 500, 200, 50, 10);
+
+function sumAll() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+console.log("Sum of Numbers is : " + x);
