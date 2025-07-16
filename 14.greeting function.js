1c1b7ea83a0b2897a6fde5ff57282b57d837302a
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2025-07-16 16:38:15 +0530

    update

--- 14.greeting function.js
+++ 14.greeting function.js
@@ -11,4 +11,13 @@ function greet1(name) {
 
 greet1("Simon"); // Call the function with a name parameter
 
-// 3. 
+// 3. calling the function without a name and if the value is undefined, print "Hello There"
+function greet2(name) {
+    if (name === undefined) {
+        console.log("Hello There");
+    } else {
+        console.log(`Hello ${name}`);
+    }
+}
+greet2(); // Call the function without a name
+greet2("Alice"); // Call the function with a name parameter 
