11b651debbfa1887331e139b07234d0b9478fa39
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2025-07-16 16:35:25 +0530

    update

---
+++ 14.greeting function.js
@@ -0,0 +1,14 @@
+// 1. create a function called greet that prints hello.
+function greet() {
+    console.log("Hello");
+}   
+greet(); // Call the function to see the output
+
+// 2. add a parameter to the function called name
+function greet1(name) {
+    console.log(`Hello ${name}`);
+}
+
+greet1("Simon"); // Call the function with a name parameter
+
+// 3. 
