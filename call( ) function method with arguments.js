485d56aba752323a6796d53c1cbd279d733285a0
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2025-07-16 16:21:08 +0530

    update

---
+++ call( ) function method with arguments.js
@@ -0,0 +1,17 @@
+const person = {
+  fullName: function(city, country) {
+    return this.firstName + " " + this.lastName + "," + city + "," + country;
+  }
+}
+
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+
+const person2 = {
+  firstName:"Mary",
+    lastName: "Doe"
+}
+
+console.log(person.fullName.call(person1, "Oslo", "Norway"));
