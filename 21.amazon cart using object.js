2727d43734c08f7068fba113d35d1df1c1c7fa26
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2025-07-18 14:17:50 +0530

    update

--- 21.amazon cart using object.js
+++ 21.amazon cart using object.js
@@ -8,14 +8,14 @@ console.log(`product1 name: ${product1.name}\nproduct1 price: ${product1.price}`
 // 2. increase the price by 500 and display
 console.log(`\nproduct2 name: ${product1.name}\nproduct2 price: ${product1.price+500}`);
 
-// 3. using bracket notation, add property('delivery time') with value('3 days'), chang the product name to lowe case and display it.
+// 3. using bracket notation, add property('delivery time') with value('3 days'), chang the product name to lowe case, repeat a string and display it.
 const product2 = {
     name: 'BasketBall',
     price: 2096,
     'delivery-time': '3 days'
 }
-
-console.log(`\nproduct2 name: ${product2.name.toLowerCase()}\nproduct2 price: ${product2.price+500}\ndelivery time: ${product2['delivery-time']}`);
+// toLowerCase(), repeat()
+console.log(`\nproduct2 name: ${product2.name.toLowerCase()}\nproduct2 price: ${product2.price+500}\ndelivery time: ${product2['delivery-time'].repeat(3)}`);
 // 4. create a function to compare the two product price and display less expensive product
 function comparePrice() {
     if(product1.price < product2.price){
